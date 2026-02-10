97b306618cc3f80e69d1133745e073e26620b6c4
Nikolas Rank <nikolas.rank@uni-ulm.de>
2026-02-10 15:35:34 +0100

    Update.

--- tests/dev.arcovia.mitigation.smt.tests/src/dev/arcovia/mitigation/smt/tests/ScalabilityTest.java
+++ tests/dev.arcovia.mitigation.smt.tests/src/dev/arcovia/mitigation/smt/tests/ScalabilityTest.java
@@ -3,6 +3,7 @@ package dev.arcovia.mitigation.smt.tests;
 import static java.util.Map.entry;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -39,11 +40,9 @@ import dev.arcovia.mitigation.sat.dsl.CNFTranslation;
 
 public class ScalabilityTest {
 
-	
-	private static final int RUNS_PER_CONFIGURATION = 100;
-	private static final long MAX_TIME_MILLIS = TimeUnit.HOURS.toMillis(1);
-	
-	
+	private static final int RUNS_PER_CONFIGURATION = 10;
+	private static final long MAX_TIME_MILLIS = TimeUnit.MINUTES.toMillis(5);
+
 	private record ScaleInput(DataFlowDiagramAndDictionary inputDfd, List<AnalysisConstraint> inputConstraints,
 			int scaleFactor) {
 	};
@@ -51,7 +50,6 @@ public class ScalabilityTest {
 	private record ScaleOutput(DataFlowDiagramAndDictionary outputDfd, List<AnalysisConstraint> outputConstraints) {
 	};
 
-	
 	// SCALERS
 	private static final Function<ScaleInput, ScaleOutput> scaleNodesAndFlows = (scaleInput) -> {
 		Scaler scaler = new Scaler(scaleInput.inputDfd);
@@ -65,8 +63,14 @@ public class ScalabilityTest {
 	};
 
 	private static final Function<ScaleInput, ScaleOutput> scaleTFGAmount = (scaleInput) -> {
+		/** Maybe scale by powers of two instead of linear
+		int scaleFactor = 0;
+		for (int i = 0; i < scaleInput.scaleFactor; i++) {
+			scaleFactor = Integer.parseInt(BigInteger.TWO.pow(i).toString());
+		}
+		*/
 		Scaler scaler = new Scaler(scaleInput.inputDfd);
-		return new ScaleOutput(scaler.scaleTFGLength(scaleInput.scaleFactor), scaleInput.inputConstraints);
+		return new ScaleOutput(scaler.scaleTFGAmount(scaleInput.scaleFactor), scaleInput.inputConstraints);
 	};
 
 	private static final Function<ScaleInput, ScaleOutput> scaleLabelsAndTypes = (scaleInput) -> {
@@ -79,7 +83,8 @@ public class ScalabilityTest {
 		Scaler scaler = new Scaler(scaleInput.inputDfd);
 		DataFlowDiagramAndDictionary dfdWithLabels = scaler.scaleLabels(scaleInput.scaleFactor);
 		List<AnalysisConstraint> allConstraints = scaleInput.inputConstraints;
-		allConstraints.addAll(scaler.scaleConstraint(1, scaleInput.scaleFactor, scaleInput.scaleFactor, 1, 1, scaleInput.scaleFactor));
+		allConstraints.addAll(scaler.scaleConstraint(1, scaleInput.scaleFactor, scaleInput.scaleFactor, 1, 1,
+				scaleInput.scaleFactor));
 		return new ScaleOutput(dfdWithLabels, allConstraints);
 	};
 
@@ -87,7 +92,8 @@ public class ScalabilityTest {
 		Scaler scaler = new Scaler(scaleInput.inputDfd);
 		DataFlowDiagramAndDictionary dfdWithLabels = scaler.scaleLabels(scaleInput.scaleFactor);
 		List<AnalysisConstraint> allConstraints = scaleInput.inputConstraints;
-		allConstraints.addAll(scaler.scaleConstraint(1, 1, 1, scaleInput.scaleFactor, scaleInput.scaleFactor, scaleInput.scaleFactor));
+		allConstraints.addAll(scaler.scaleConstraint(1, 1, 1, scaleInput.scaleFactor, scaleInput.scaleFactor,
+				scaleInput.scaleFactor));
 		return new ScaleOutput(dfdWithLabels, allConstraints);
 	};
 
@@ -99,8 +105,8 @@ public class ScalabilityTest {
 		return new ScaleOutput(dfdWithLabels, allConstraints);
 	};
 
-	
-	//Measure runtime of all TUHH Models with defined scaler and increasing scale Factor until either tool exceeds runtime of 1h.
+	// Measure runtime of all TUHH Models with defined scaler and increasing scale
+	// Factor until either tool exceeds runtime of 1h.
 	public void scalabilityTest(Function<ScaleInput, ScaleOutput> scaleFunc, String name) throws Exception {
 		try {
 
@@ -139,11 +145,15 @@ public class ScalabilityTest {
 							SolvingResult solvingResult = Main.run(scaleOutput.outputDfd, scaleOutput.outputConstraints,
 									null);
 							long after = System.currentTimeMillis();
-							/**
-							 * if (!solvingResult.satisfiable() ||
-							 * Util.countViolations(solvingResult.repairedDFD(), constraint) > 0) {
-							 * System.out.println("Found error in SMT"); System.exit(1); };
-							 */
+
+							if (j == 0 && (!solvingResult.satisfiable()
+									|| Util.countViolations(solvingResult.repairedDFD(), constraint) > 0)) {
+								System.out.println("Found error in SMT");
+								scaleOutput.outputConstraints.forEach(System.out::println);
+								System.exit(1);
+							}
+							;
+
 							long smtRuntime = after - before;
 							smtRuntimes.add(smtRuntime);
 						}
@@ -155,11 +165,14 @@ public class ScalabilityTest {
 							ScaleOutput scaleOutput = scaleFunc.apply(scaleInput);
 							RepairResult repairResult = runRepair(scaleOutput.outputDfd, false,
 									scaleOutput.outputConstraints, minCosts);
-							/**
-							 * if (repairResult.violationsAfter() > 0 ||
-							 * Util.countViolations(repairResult.repairedDfd, constraint) > 0) {
-							 * System.out.println("Found error in SAT"); System.exit(1); }
-							 */
+
+							if (j == 0 && (repairResult.violationsAfter() > 0
+									|| Util.countViolations(repairResult.repairedDfd, constraint) > 0)) {
+								System.out.println("Found error in SAT");
+								scaleOutput.outputConstraints.forEach(System.out::println);
+								System.exit(1);
+							}
+
 							long satRuntime = repairResult.runtimeInMilliseconds;
 							satRuntimes.add(satRuntime);
 						}
@@ -170,7 +183,7 @@ public class ScalabilityTest {
 				ScalabilityResult runtimeResult = new ScalabilityResult(scale, runtimeSmtCurrRun, runtimeSatCurrRun,
 						smtRuntimes, satRuntimes);
 				scalabilityResults.add(runtimeResult);
-				System.out.println("Scale " + scale);
+				System.out.println("Scaled " + name + " by factor " + scale);
 				System.out.println("Total Runtime SMT " + runtimeSmtCurrRun);
 				System.out.println("Total Runtime SAT " + runtimeSatCurrRun);
 				scale++;
@@ -199,14 +212,49 @@ public class ScalabilityTest {
 	}
 
 	@Test
-	public void testAllForScalability() throws Exception {
-		scalabilityTest(scaleNodesAndFlows, "nodesAndFlows");
-		scalabilityTest(scaleTFGLength, "tfgLength");
+	public void testTFGAmount() throws Exception {
 		scalabilityTest(scaleTFGAmount, "tfgAmount");
+	}
+
+	@Test
+	public void testTFGLength() throws Exception {
+		scalabilityTest(scaleTFGLength, "tfgLength");
+	}
+
+	@Test
+	public void testLabelsAndTypes() throws Exception {
+		scalabilityTest(scaleLabelsAndTypes, "labelsAndTypes");
+	}
+
+	@Test
+	public void testLabelsInConstraintBeforeNeverFlows() throws Exception {
+		scalabilityTest(scaleLabelsInConstraintBeforeNeverFlows, "labelsBeforeNeverFlows");
+	}
+
+	@Test
+	public void testLabelsInConstraintAfterNeverFlows() throws Exception {
+		scalabilityTest(scaleLabelsInConstraintAfterNeverFlows, "labelsAfterNeverFlows");
+	}
+
+	@Test
+	public void testNumConstraints() throws Exception {
+		scalabilityTest(scaleNumConstraints, "numConstraints");
+	}
+
+	@Test
+	public void testNodesAndFlows() throws Exception {
+		scalabilityTest(scaleNodesAndFlows, "nodesAndFlows");
+	}
+
+	@Test
+	public void testAllForScalability() throws Exception {
+		scalabilityTest(scaleTFGAmount, "tfgAmount");
+		scalabilityTest(scaleTFGLength, "tfgLength");
 		scalabilityTest(scaleLabelsAndTypes, "labelsAndTypes");
 		scalabilityTest(scaleLabelsInConstraintBeforeNeverFlows, "labelsBeforeNeverFlows");
 		scalabilityTest(scaleLabelsInConstraintAfterNeverFlows, "labelsAfterNeverFlows");
 		scalabilityTest(scaleNumConstraints, "numConstraints");
+		scalabilityTest(scaleNodesAndFlows, "nodesAndFlows");
 	}
 
 	private RepairResult runRepair(DataFlowDiagramAndDictionary dfd, Boolean store,
