4c436409a4aa11d2caa7c319c42e2b4b846c3dd7
Nikolas Rank <nikolas.rank@uni-ulm.de>
2026-02-25 17:55:19 +0100

    Remove scripts.

--- bundles/dev.arcovia.mitigation.smt/src/dev/arcovia/mitigation/smt/print.sh
+++
@@ -1,9 +0,0 @@
-outfile="output.txt"
-: > "$outfile"
-
-find . -type f ! -name "$outfile" -print0 |
-while IFS= read -r -d '' f; do
-  echo "===== ${f#./} =====" >> "$outfile"
-  grep -vE '^[[:space:]]*$|^[[:space:]]*(input|import)\b' "$f" >> "$outfile"
-  echo >> "$outfile"
-done
